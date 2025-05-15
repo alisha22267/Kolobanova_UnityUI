6c4f4d3f827663e3616fce94eeb2206268d8cd65
Kolobanova <alinakolobanova22@gmail.com>
2025-05-15 20:36:05 +0300

    Pievonts skaņa pie tēla.

---
+++ Assets/scripts/DollSource_scripts.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollSource_scripts : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] audioClips;
+
+    public void OnImage()
+    {
+        audioSource.PlayOneShot(audioClips[Random.Range(0, audioClips.Length)]);
+    }
+    public void StopSound()
+    {
+        audioSource.Stop();
+    }
+}
